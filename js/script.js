e1b8b56b8b118211fadf0f4956b8f489f1244df6
Santos Gonzalez <smg061@shsu.edu>
2021-04-13 20:28:57 -0500

    added functional Nutritionix search in script.js

--- js/script.js
+++ js/script.js
@@ -1,58 +1,7 @@
-var API_KEY = "c5de6033238da0ac0e14b9584fa42144";
-var AP_ID = "9ca4ed8a"
-var FETCH_URL =  "https://api.edamam.com/api/nutrition-details?app_id="+AP_ID + "&app_key="+ API_KEY;
-
-// recipe to submit to nutrition analysis API
-var recipe = {
-    title: "Fresh Ham Roasted With Rye Bread and Dried Fruit Stuffing",
-    prep: "1. Have your butcher bone and butterfly the ham and score the fat in a diamond pattern. ...",
-    yield: "About 15 servings",
-    ingr: [
-      "1 fresh ham, about 18 pounds, prepared by your butcher (See Step 1)",
-      "7 cloves garlic, minced",
-      "1 tablespoon caraway seeds, crushed",
-      "4 teaspoons salt",
-      "Freshly ground pepper to taste",
-      "1 teaspoon olive oil",
-      "1 medium onion, peeled and chopped",
-      "3 cups sourdough rye bread, cut into 1/2-inch cubes",
-      "1 1/4 cups coarsely chopped pitted prunes",
-      "1 1/4 cups coarsely chopped dried apricots",
-      "1 large tart apple, peeled, cored and cut into 1/2-inch cubes",
-      "2 teaspoons chopped fresh rosemary",
-      "1 egg, lightly beaten",
-      "1 cup chicken broth, homemade or low-sodium canned"
-    ]
-  }
 
 
-// function that fetches nutrition details of the recipe detailed above
-fetch (FETCH_URL, 
-    {
-        method: "POST",
-        headers: {"Content-Type": "application/json;charset=UTF-8"}, // specify the format of the recipe submission in the header
-        body: JSON.stringify(recipe) // submit the recipe in stringified form in the body
-    })
-.then(res => {
-    if(res.ok){
-        console.log("SUCCESS")
-    
-    } else {
-        console.log("not success")
-    }
-    // return the response
-    return res.json()
-})
-.then (data => {
-    console.log(data)
-})
 
-
-/* TODO: Add an search bar element to the HTML document that parses search parameters and displays the results dynamically */
-// recipe fetch 
-
-// reassinging the variables from above to reuse the parameters from the function above
-var parameters = "?q=chicken";
+// API call variables
 
 AP_ID = "8644d665";
 
@@ -60,13 +9,11 @@ API_KEY = "64d713a99c6d8aab9d04c626a6c6ab94";
 
 FETCH_URL = "https://api.edamam.com/search?q=chicken&mealType=lunch&app_id=" + AP_ID + "&app_key=" + API_KEY;
 
-
-function fetchRecipe(FETCH_URL){
+function fetchEdamamRecipe(FETCH_URL) {
     fetch (FETCH_URL, 
         {
             method: "POST",
             headers: {"Content-Type": "application/json;charset=UTF-8"},
-            
         })
 
     .then(res => {
@@ -81,8 +28,130 @@ function fetchRecipe(FETCH_URL){
 
     .then (data => {
         console.log(data)
+        for (var i = 0; i < 5; i++)
+        {
+            var recipeName = data.hits[i].recipe.label;
+            var calories = data.hits[i].recipe.calories.toFixed(0);
+            var img = data.hits[i].recipe.image;
+            var foodCard = createFoodCard(recipeName, calories, img);
+            $("#recipe-results").append(foodCard);
+
+        }
     })
 }
 
 
-fetchRecipe(FETCH_URL);
+
+// construction of search bar element to search for recipes
+var searchBox = $("<div>", {class: "field has-addons"});
+var searchField = $("<div>", {class: "control"});
+var textInput = $("<input>", {class: "input", type: "text"});
+var submitButtonDiv = $("<div>", {class: "control"});
+var submitButton = $("<button>", {class: "button is-primary", id: "submit-search-edamam"});
+// put all the html elements together
+searchField.append(textInput);
+submitButtonDiv.append(submitButton);
+searchBox.append(searchField);
+searchBox.append(submitButtonDiv);
+// append the searchbox to the test div on the HTML
+$("#test-div").append(searchBox); 
+
+// this function extracts the text from the search box, parses an appropriate fetch URL and returns it
+function getEdamamFetchURL()
+{
+    var searchText = $("input").val(); // get the value of the input text
+    return "https://api.edamam.com/search?" + "q=" + searchText.trim() + "&app_id=" + AP_ID + "&app_key=" + API_KEY; // return appropriate URL to fetch
+
+}
+
+// this function creates a card element with the photo, name, and calories of a recipe
+function createFoodCard(recipeName, calories, img)
+{
+     //containers for the food card elements
+     var headerEl = $("<h5>", {class: "card-header"})    
+     var cardEl = $("<div>", {class: "card", id: "food-card"})
+     var bodyEl = $("<div>", {class: ""})
+     var columnEl = $("<div>", {class:"column", style: "width: 2rem;"});
+     imgEl = $("<img>", {src:img, class: "card-image"})
+    // set the element attributes according to the function parameters
+     headerEl.text(recipeName);
+     var caloriesEl = $("<p>", {class: ""});
+     caloriesEl.text("calories: " +  calories);
+     // put together all the elements
+     bodyEl.append(headerEl);
+     bodyEl.append(imgEl)
+     bodyEl.append(caloriesEl);
+     cardEl.append(bodyEl)
+     columnEl.append(cardEl);
+     // return the food card element
+     return columnEl
+
+}
+
+$("#submit-search-edamam").on("click", ()=> 
+{
+    // clear the html of the area where the food results are displayed
+    $("#recipe-results").html("");
+    //var fetchURL = getEdamamFetchURL();
+    //fetchEdamamRecipe(fetchURL);
+    var fetchURL = getNutritionixFoodFetchURL();
+    fetchNutritionixFood(fetchURL);
+
+})
+
+/*
+    Here starts code related to the nutritionix API call
+
+*/
+
+var fetchURL = "https://trackapi.nutritionix.com/v2/search/instant?query=grilled cheese"
+
+// this function fetches data from the nutritionix API. it already has the appropriate headers built into it
+
+function fetchNutritionixFood(FETCH_URL){
+    fetch (FETCH_URL, 
+        {
+            method: "GET",
+            headers: {
+            "x-app-id": "01a2941e" ,
+            "x-app-key": "b6d5b488c725bb0cbf2ff56eb72183c5",
+            "Content-Type": "application/json"
+            },
+
+        })
+
+    .then(res => {
+        if(res.ok){
+            console.log("SUCCESS")
+        
+        } else {
+            console.log("not success")
+        }
+        return res.json()
+    })
+
+    .then (data => {
+        console.log(data)
+        
+        for (var i = 0; i < 5; i++)
+        {
+            var recipeName = data.branded[i].brand_name_item_name;
+            var calories = data.branded[i].nf_calories.toFixed(0);
+            var img = data.branded[i].photo.thumb;
+            var foodCard = createFoodCard(recipeName, calories, img);
+            $("#recipe-results").append(foodCard);
+
+        }
+        
+        
+  
+    })
+}
+
+function getNutritionixFoodFetchURL()
+{
+    var searchText = $("input").val(); // get the value of the input text
+    return "https://trackapi.nutritionix.com/v2/search/instant?query=" + searchText // return appropriate URL to fetch
+
+}
+
