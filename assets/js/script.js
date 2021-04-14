85600b689d1e8e382463261b299b8075b0cbcdbb
Avi Mancillas <avmancillas@gmail.com>
2021-04-13 19:27:37 -0500

    added login page

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ fetch ('https://api.edamam.com/search?q=mealType')
 })
 .then(data => console.log(data))
 .catch(error => console.log("Error"))
+
+var searchFormEl = document.querySelector('#search-form');
+
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
+
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
